c5252beeab19e3b7b4a6b75afb9633e80795c157
phpsystems <phpsystems@users.noreply.github.com>
2023-04-05 11:15:55 +0100

    Update owserver.sh

--- rootfs/etc/cont-init.d/owserver.sh
+++ rootfs/etc/cont-init.d/owserver.sh
@@ -7,7 +7,11 @@ device=$(bashio::config 'device')
 if bashio::config.has_value 'device' ;then
     bashio::log.info "Configuring ${device} device"
     device=$(bashio::string.replace ${device} '/' '\/')
-    sed -i "s/%%device%%/device = ${device}/g" /etc/owfs.conf
+    if [ '${device}' == 'usb' ]; then
+        sed -i "s/%%device%%/usb/g" /etc/owfs.conf
+    else
+        sed -i "s/%%device%%/device = ${device}/g" /etc/owfs.conf
+    fi
 else
     bashio::log.info "Configuring fake device"
     sed -i "s/%%device%%/FAKE = DS18B20,DS2405/g" /etc/owfs.conf
