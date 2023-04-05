093395f5a495465cd49769f9a6572911f615631c
phpsystems <phpsystems@users.noreply.github.com>
2023-04-05 18:08:03 +0100

    Update owserver.sh
    Locally tested now. appears to work for me.

--- rootfs/etc/cont-init.d/owserver.sh
+++ rootfs/etc/cont-init.d/owserver.sh
@@ -7,7 +7,7 @@ device=$(bashio::config 'device')
 if bashio::config.has_value 'device' ;then
     bashio::log.info "Configuring ${device} device"
     device=$(bashio::string.replace ${device} '/' '\/')
-    if [ '${device}' == 'usb' ]; then
+    if bashio::var.equals "${device}" "usb" ;then
         sed -i "s/%%device%%/usb/g" /etc/owfs.conf
     else
         sed -i "s/%%device%%/device = ${device}/g" /etc/owfs.conf
