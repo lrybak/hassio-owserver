fe56d71962614cfc0fd47109e2ced0771d7bd8c0
Łukasz Rybak <lukasz.rybak@gmail.com>
2023-04-24 23:55:47 +0200

    temperature scale selector

--- rootfs/etc/cont-init.d/owserver.sh
+++ rootfs/etc/cont-init.d/owserver.sh
@@ -2,10 +2,11 @@
 
 declare device
 declare device_type
+declare temperature_scale
 device=$(bashio::config 'device')
 device_type=$(bashio::config 'device_type')
+temperature_scale=$(bashio::config 'temperature_scale')
 
-value=$(bashio::config 'Connection_Type')
 if  bashio::var.equals "${device_type}" "fake"; then
     bashio::log.info "Configuring fake device"
     sed -i "s/%%device%%/FAKE = DS18B20,DS2405/g" /etc/owfs.conf
@@ -17,3 +18,6 @@ else
     device=$(bashio::string.replace ${device} '/' '\/')
     sed -i "s/%%device%%/device = ${device}/g" /etc/owfs.conf
 fi
+
+bashio::log.info "Configuring temperature scale: ${temperature_scale}"
+sed -i "s/%%temperature_scale%%/${temperature_scale}/g" /etc/owfs.conf
